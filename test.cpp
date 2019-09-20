fe7cbb0e5d7e215ecf8edc58ddc23ea02725595b
y21 <artur.fiedler54@gmail.com>
2019-09-21 01:14:35 +0200

    improve test...

--- test.cpp
+++ test.cpp
@@ -4,7 +4,8 @@
 int main(int argc, char** argv) {
 	optparse::option* opts = optparse::parse(argc, argv, "abc");
 	for (int i = 0;; ++i) {
-		if (opts[i].name == NULL) break;
+		if (!opts[i].name) break;
 		printf("Found option: %c\n", opts[i].name);
 	}
+	free(opts);
 }

2de8853ce8a458a08f908c3f3ec17018cc3f1f7d
y21 <artur.fiedler54@gmail.com>
2019-09-21 01:16:25 +0200

    add comments

--- test.cpp
+++ test.cpp
@@ -2,10 +2,18 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
+	// Store result of optparse::parse in a variable
+	// to iterate over it
 	optparse::option* opts = optparse::parse(argc, argv, "abc");
+	
 	for (int i = 0;; ++i) {
+		// Break out of the loop if !opts[i].name: no more flags
 		if (!opts[i].name) break;
+		
+		// opts[i].name holds the option name, while opts[i].value holds the value.
 		printf("Found option: %c\n", opts[i].name);
 	}
+	
+	// Free allocated memory
 	free(opts);
 }

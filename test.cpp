0eb1a8b8ee4d5a277443d5fa7565ecbefb49c7b3
y21 <artur.fiedler54@gmail.com>
2019-09-21 00:41:30 +0200

    add test commit

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <optparse/parser.hpp>
+#include <stdio.h>
+int main(int argc, char** argv) {
+	optparse::option* opts = optparse::parse(argc, argv, "abc");
+	for (int i = 0;; ++i) {
+		if (opts[i].name == NULL) break;
+		printf("Found option: %c\n", opts[i].name);
+	}
+}

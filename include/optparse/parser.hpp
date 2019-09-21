b685dbb64eeccd3de157e0bceffb866d8fdb8288
y21 <artur.fiedler54@gmail.com>
2019-09-21 15:05:27 +0200

    add support for option value

--- include/optparse/parser.hpp
+++ include/optparse/parser.hpp
@@ -2,12 +2,12 @@
 #define __OPTPARSE__
 #define __OPTPARSE_DEFAULT_BUFF__ 0x10
 #define __OPTPARSE_OPT_COUNT__ 0x8
-#include<stdlib.h>
-#include<cstring>
+#include <stdlib.h>
+#include <cstring>
 namespace optparse {
 	typedef struct {
 		char name;
-		char value[__OPTPARSE_DEFAULT_BUFF__];
+		char* value;
 	} option;
 	
 	option* parse(int __argc, char** __argv, const char* __opt) {
@@ -17,9 +17,26 @@ namespace optparse {
 		for (int i = 0; i < __argc; ++i) {
 			const int len = strlen(__argv[i]);
 			for (int j = 0; j < len; ++j) {
-				if (__argv[i][j] == '-' && j + 1 <= len && strchr(__opt, __argv[i][j + 1]) != NULL && __argv[i][j + 1] != ':') {
+				const char current_char = __argv[i][j];
+				const char next_char = __argv[i][j+1];
+				if (current_char == '-' && j + 1 <= len && strchr(__opt, next_char) != NULL && next_char != ':') {
 					option opt;
 					opt.name = __argv[i][j + 1];
+
+					if (__argv[i + 1] && __argv[i + 1][0] != '-') {
+						int opt_val_index = 0;
+						int opt_val_limit = __OPTPARSE_DEFAULT_BUFF__;
+						opt.value = (char*)malloc(sizeof(char) * opt_val_limit);
+						for (int k = 0; k < strlen(__argv[i + 1]); ++k) {
+							if (opt_val_index >= opt_val_limit) {
+								opt.value = (char*)realloc(opt.value,  opt_val_limit *= 2);
+							}
+							opt.value[opt_val_index++] = __argv[i + 1][k];
+						}
+						opt.value[opt_val_index] = '\0';
+					}
+
+
 					if (optCount >= optLimit)
 						options = (option*) realloc(options, sizeof(option) * (optLimit *= 2));
 					options[optCount++] = opt;
@@ -27,6 +44,12 @@ namespace optparse {
 			}
 		}
 		return options;
+	}
+	
+	option* parse(char* __argv, const char* __opt) {
+		char** __argvP = (char**) malloc(sizeof(char) * strlen(__argv));
+		__argvP[0] = __argv;
+		return parse(1, __argvP, __opt);
 	}
 }
 

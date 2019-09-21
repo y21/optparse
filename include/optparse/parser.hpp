eb38d41d61ab494b79111947e433fdfdddd57da9
y21 <artur.fiedler54@gmail.com>
2019-09-21 16:50:37 +0200

    add parser class and move function

--- include/optparse/parser.hpp
+++ include/optparse/parser.hpp
@@ -9,42 +9,61 @@ namespace optparse {
 		char name;
 		char* value;
 	} option;
-	
-	option* parse(int __argc, char** __argv, const char* __opt) {
-		int optCount = 0;
-		int optLimit = __OPTPARSE_OPT_COUNT__;
-		option* options = (option*)malloc(sizeof(option) * optLimit);
-		for (int i = 0; i < __argc; ++i) {
-			const int len = strlen(__argv[i]);
-			for (int j = 0; j < len; ++j) {
-				const char current_char = __argv[i][j];
-				const char next_char = __argv[i][j+1];
-				if (current_char == '-' && j + 1 <= len && strchr(__opt, next_char) != NULL && next_char != ':') {
-					option opt;
-					opt.name = __argv[i][j + 1];
 
-					if (__argv[i + 1] && __argv[i + 1][0] != '-') {
-						int opt_val_index = 0;
-						int opt_val_limit = __OPTPARSE_DEFAULT_BUFF__;
-						opt.value = (char*)malloc(sizeof(char) * opt_val_limit);
-						for (int k = 0; k < strlen(__argv[i + 1]); ++k) {
-							if (opt_val_index >= opt_val_limit) {
-								opt.value = (char*)realloc(opt.value,  opt_val_limit *= 2);
+	class parser {
+		private:
+			int option_count = 0;
+			int option_limit = __OPTPARSE_OPT_COUNT__;
+			option* options = (option*) malloc(sizeof(option) * option_limit);
+		public:
+			parser(int __argc, char** __argv, char* __opt) {
+				for (int i = 0; i < __argc; ++i) {
+					const int len = strlen(__argv[i]);
+					for (int j = 0; j < len; ++j) {
+						const char current_char = __argv[i][j];
+						const char next_char = __argv[i][j+1];
+						if (current_char == '-' && j + 1 <= len && strchr(__opt, next_char) != NULL && next_char != ':') {
+							option opt;
+							opt.name = __argv[i][j + 1];
+
+							if (__argv[i + 1] && __argv[i + 1][0] != '-') {
+								int opt_val_index = 0;
+								int opt_val_limit = __OPTPARSE_DEFAULT_BUFF__;
+								opt.value = (char*)malloc(sizeof(char) * opt_val_limit);
+								for (int k = 0; k < strlen(__argv[i + 1]); ++k) {
+									if (opt_val_index >= opt_val_limit) {
+										opt.value = (char*)realloc(opt.value,  opt_val_limit *= 2);
+									}
+									opt.value[opt_val_index++] = __argv[i + 1][k];
+								}
+								opt.value[opt_val_index] = '\0';
 							}
-							opt.value[opt_val_index++] = __argv[i + 1][k];
+
+
+							if (this->option_count >= this->option_limit)
+								this->options = (option*) realloc(this->options, sizeof(option) * (this->option_limit *= 2));
+							this->options[this->option_count++] = opt;
 						}
-						opt.value[opt_val_index] = '\0';
 					}
-
-
-					if (optCount >= optLimit)
-						options = (option*) realloc(options, sizeof(option) * (optLimit *= 2));
-					options[optCount++] = opt;
 				}
 			}
-		}
-		return options;
-	}
+			
+			int get_option_count() const {
+				return this->option_count;
+			}
+
+			int get_option_limit() const {
+				return this->option_limit;
+			}
+
+			option* get_options() const {
+				return this->options;
+			}
+
+			~parser() {
+				free(this->options);
+			}
+	};
 }
 
 #endif

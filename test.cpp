1eeed2052662e6eaa94347d50e0dee3a906a4d88
Timo <artur.fiedler54@gmail.com>
2019-10-08 19:28:33 +0200

    add comments

--- test.cpp
+++ test.cpp
@@ -5,10 +5,13 @@
 
 int main(int argc, char** argv) {
 
+	// Create instance of optparse::parser
 	optparse::parser parser(argc, argv, "*");
 
+	// Get options
 	std::vector<optparse::option> opts = parser.get_options();
 
+	// Iterate over options
 	for (int i = 0; i < opts.size(); ++i) {
 		std::cout << opts.at(i).name << ": " << opts.at(i).value << "\n";
 	}
